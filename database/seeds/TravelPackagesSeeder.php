dd2313c8024832598da7557c0417e039df2ff5a0
Simone Mauro <simon.mauro@virgilio.it>
2022-03-31 17:04:36 +0200

    Bonus, generazione dati casuali tramite libreria Faker

--- database/seeds/TravelPackagesSeeder.php
+++ database/seeds/TravelPackagesSeeder.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Database\Seeder;
 
+use Faker\Generator as Faker;
 use App\Travel;
+
+require_once 'vendor/autoload.php';
 
 class TravelPackagesSeeder extends Seeder
 {
@@ -11,10 +14,10 @@ class TravelPackagesSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
         //richiamo il file Model, per creare un nuovo elemento della tabella
-        $travel = new Travel();
+        /* $travel = new Travel();
 
         $travel["nome"] = "vacanza tropicale deluxe";
         $travel["descrizione"] = "Vacanza rilassante dopo la fine del corso";
@@ -23,7 +26,23 @@ class TravelPackagesSeeder extends Seeder
         $travel["numero_recensioni"] = 220;
         $travel["numero_giorni"] = 7;
 
-        $travel->save();
+        $travel->save(); */
+
+
+        //BONUS dati generati casualmente tramite la classe Faker
+        for($i = 0; $i < 10; $i++){
+            //richiamo il file Model, per creare un nuovo elemento della tabella
+            $travel = new Travel();
+
+            $travel->nome = $faker->name();
+            $travel->descrizione = $faker->text();
+            $travel->prezzo = $faker->randomNumber(4, true);
+            $travel->indirizzo = $faker->sentence();
+            $travel->numero_recensioni = $faker->randomNumber(3, false);
+            $travel->numero_giorni = $faker->randomNumber(2, true);
+
+            $travel->save();
+        }
 
     }
 }
